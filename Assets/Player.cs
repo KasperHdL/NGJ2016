584ed5cadcb58e9cf028de9f4ded5393e4418678
Itai Yavin <Itai.yavin@gmail.com>
2016-04-09 15:33:41 +0200

    First android implementation (just simple button) plus all dependencies and plugins

---
+++ Assets/Player.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+using System.Collections;
+
+public class Player : MonoBehaviour {
+
+	public Canvas canvas;
+	public Image panelImage;
+
+	private int screenX;
+	private int screenY;
+
+	// Use this for initialization
+	void Start () {
+		screenX = Screen.width;
+		screenY = Screen.height;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetTouch(0).phase == TouchPhase.Began && Input.GetTouch(0).position.x > screenX/2){
+			ButtonPressed();
+		}
+	}
+
+	public void ButtonPressed(){
+		float r;
+		float g;
+		float b;
+
+		r = Random.Range(0, 1f);
+		g = Random.Range(0, 1f);
+		b = Random.Range(0, 1f);
+
+		Color col = new Color(r,g,b);
+
+		panelImage.color = col;
+	}
+}
