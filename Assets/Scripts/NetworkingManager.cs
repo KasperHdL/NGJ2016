6abf30f099158db3a72b157ae35d8fb9bc92d8e4
KasperHdL <kasper@honnens.dk>
2016-04-09 13:02:49 +0200

    Simple Networking

---
+++ Assets/Scripts/NetworkingManager.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+using Photon;
+
+public class NetworkingManager : PunBehaviour {
+
+	// Use this for initialization
+	void Start () {
+            PhotonNetwork.ConnectUsingSettings("0.1");
+    }
+ 
+    void OnGUI()
+    {
+        GUILayout.Label(PhotonNetwork.connectionStateDetailed.ToString());
+    }
+	
+	public override void OnJoinedLobby()
+    {
+        PhotonNetwork.JoinRandomRoom();
+    }
+    void OnPhotonRandomJoinFailed()
+    {
+        PhotonNetwork.CreateRoom(null);
+    }
+    void OnJoinedRoom()
+    {
+        GameObject go = PhotonNetwork.Instantiate("PlayerController", Vector3.zero, Quaternion.identity, 0);
+        PlayerController controller = go.GetComponent<PlayerController>();
+        controller.enabled = true;
+        controller.controlledLocally = true;
+    }
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+
+    public bool buttonDown = false;
+    public bool controlledLocally = false;
+
+    void Update(){
+        if(controlledLocally)
+            buttonDown = Input.GetKey(KeyCode.Space);
+    }
+    
+          // in an "observed" script:
+    public void OnPhotonSerializeView(PhotonStream stream, PhotonMessageInfo info)
+    {
+        if (stream.isWriting)
+        {
+            stream.SendNext(buttonDown);
+        }
+        else
+        {
+           buttonDown = (bool) stream.ReceiveNext();
+            Debug.Log("Receiving: " + buttonDown);
+        }
+        
+        
+    }
+        
+}
