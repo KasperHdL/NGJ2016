b9d0b3c9db6181993fd6fc22fd4a2e54687381e1
Botched135 <rasmusbons@gmail.com>
2016-04-09 16:40:40 +0200

    Left-to-right Progression. Camera moves and spawns

---
+++ Assets/Left-to-Right Progression/CameraScript.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraScript : MonoBehaviour
+{
+    private float i;
+    private float countdownTime;
+    public enum gameState
+    {
+        Menu,
+        Begin,
+        GameTime,
+        EndSlow
+    }
+    public gameState _currentState = gameState.Begin;
+    
+    // Update is called once per frame
+    void Update()
+    {
+        if (_currentState == gameState.Menu)
+        {
+
+        }
+        else if (_currentState == gameState.Begin)
+        {
+            //Insert initial countdown
+            i += Time.deltaTime;
+            if (i >= 3)
+                _currentState = gameState.GameTime;
+
+        }
+        else if (_currentState == gameState.GameTime)
+        {
+            CameraMovement();
+        }
+        else if (_currentState == gameState.EndSlow)
+        {
+            //MLG theme+zoom+slowmotion
+        }
+        else
+            throw new System.Exception();
+    }
+    public void CameraMovement()
+    {
+        transform.position = new Vector3(transform.position.x + Time.deltaTime, transform.position.y, transform.position.z);
+    }
+}
--- Assets/Left-to-Right Progression/ObjectSpawn.cs
+++ Assets/Left-to-Right Progression/ObjectSpawn.cs
@@ -2,14 +2,44 @@
 using System.Collections;
 
 public class ObjectSpawn : MonoBehaviour {
-
+    [SerializeField]
+    private GameObject[] setOfGrapPoints = new GameObject[1];
+    private GameObject[] ActiveGrapPoints = new GameObject[5];
+    private Vector3 _CameraPrePos, _currentCameraPos;
+    private Bounds _cameraBounds;
 	// Use this for initialization
-	void Start () {
-	
+	void Awake () {
+        _cameraBounds = CameraSizing.CameraBounds(this.GetComponent<Camera>());
+        _currentCameraPos = _cameraBounds.center;
+        _CameraPrePos = _cameraBounds.center;
+        for (int i = 2; i < ActiveGrapPoints.Length; i++)
+        {
+
+        }
 	}
 	
 	// Update is called once per frame
 	void Update () {
+        _cameraBounds = CameraSizing.CameraBounds(this.GetComponent<Camera>());
+        _currentCameraPos = _cameraBounds.center;
+           if (_CameraPrePos.x < (_currentCameraPos.x-_cameraBounds.extents.x*1.5))
+        {
+            ArrayManager(Instantiate(setOfGrapPoints[0],
+                                    new Vector2((_currentCameraPos.x + _cameraBounds.extents.x) * 2, _currentCameraPos.y),
+                                    setOfGrapPoints[0].transform.rotation), 
+                                    ActiveGrapPoints);
+            
+            _CameraPrePos = _currentCameraPos;
+        }
 	
 	}
+    void ArrayManager(Object other, GameObject[] array)
+    {
+        Destroy(array[0]);
+        for (int i = 1; i < array.Length; i++)
+        {
+            array[i - 1] = array[i];
+        }
+        array[array.Length - 1] = other as GameObject;
+    }
 }
