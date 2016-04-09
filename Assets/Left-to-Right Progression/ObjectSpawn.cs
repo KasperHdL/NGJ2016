2e9289fc46d0fb0c073634b6fac01f2e63094fc3
Botched135 <rasmusbons@gmail.com>
2016-04-09 21:07:08 +0200

    Left-to-right progression with start. As good as done

--- Assets/Left-to-Right Progression/ObjectSpawn.cs
+++ Assets/Left-to-Right Progression/ObjectSpawn.cs
@@ -3,30 +3,39 @@ using System.Collections;
 
 public class ObjectSpawn : MonoBehaviour {
     [SerializeField]
-    private GameObject[] setOfGrapPoints = new GameObject[1];
+    private GameObject[] setOfGrapPoints = new GameObject[8];
     private GameObject[] ActiveGrapPoints = new GameObject[5];
     private float sizeToVectorFactor = 3.5536f;
     private Vector3 _CameraPrePos, _currentCameraPos;
     private Bounds _cameraBounds;
-	// Use this for initialization
+    private int index; 
+   
 	void Awake () {
         _cameraBounds = CameraSizing.CameraBounds(this.GetComponent<Camera>());
         _currentCameraPos = _cameraBounds.center;
         _CameraPrePos = _cameraBounds.center;
-        for (int i = 2; i < ActiveGrapPoints.Length; i++)
-        {
-
-        }
+        ArrayManager(Instantiate(setOfGrapPoints[6],
+                                    new Vector2(_cameraBounds.center.x, _cameraBounds.center.y),
+                                    setOfGrapPoints[0].transform.rotation),
+                                    ActiveGrapPoints);
+        ArrayManager(Instantiate(setOfGrapPoints[1],
+                                    new Vector2((_cameraBounds.center.x + (_cameraBounds.extents.x * (sizeToVectorFactor/2))), _cameraBounds.center.y),
+                                    setOfGrapPoints[0].transform.rotation),
+                                    ActiveGrapPoints);
+        ArrayManager(Instantiate(setOfGrapPoints[2],
+                                    new Vector2((_cameraBounds.center.x + (_cameraBounds.extents.x * sizeToVectorFactor)), _cameraBounds.center.y),
+                                    setOfGrapPoints[0].transform.rotation),
+                                    ActiveGrapPoints);
 	}
-	
-	// Update is called once per frame
+
 	void Update () {
         _cameraBounds = CameraSizing.CameraBounds(this.GetComponent<Camera>());
+        
            if (_CameraPrePos.x < (_cameraBounds.center.x- (_cameraBounds.extents.x/2) * sizeToVectorFactor))
         {
             _CameraPrePos = _cameraBounds.center;
-            
-            ArrayManager(Instantiate(setOfGrapPoints[0],
+            index = Random.Range(1, setOfGrapPoints.Length);
+            ArrayManager(Instantiate(setOfGrapPoints[index],
                                     new Vector2((_cameraBounds.center.x + (_cameraBounds.extents.x * sizeToVectorFactor)), _cameraBounds.center.y),
                                     setOfGrapPoints[0].transform.rotation), 
                                     ActiveGrapPoints);
