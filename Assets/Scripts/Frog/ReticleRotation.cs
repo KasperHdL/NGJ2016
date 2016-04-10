87dbb76f22ce0f2b4f205e193b98c0ea31ee4cd0
Itai Yavin <Itai.yavin@gmail.com>
2016-04-10 03:12:33 +0200

    Made new frog prefag with aim reticle. Aim reticle requires a direction vector to be functional. - Aim reticle script holds a SetDirection(Vector2) function, which sets its direction, which is used to calculate its rotation.

---
+++ Assets/Scripts/Frog/ReticleRotation.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ReticleRotation : MonoBehaviour {
+	//public float length;
+
+	private Vector2 direction;
+	private float lastRotation;
+	// Use this for initialization
+	void Start () {
+		lastRotation = 0;
+		direction = new Vector2(0,1);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		SetReticle();
+	}
+
+	public void SetDirection(Vector2 dir){
+		direction = dir;
+		SetReticle();
+	}
+
+	public void SetReticle(){
+		//transform.position = direction * length;
+        Vector2 tempRight = new Vector2(0, 1);
+		float rotationTemp = Vector2.Angle(direction, tempRight);
+		transform.Rotate(Vector3.forward, rotationTemp-lastRotation, Space.Self); 
+		lastRotation = rotationTemp;
+	}
+
+}
